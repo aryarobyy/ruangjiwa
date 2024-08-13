14f5510d1a5190fc46af572a4bfadf1570eec94c
rolis1 <rolisliu0@gmail.com>
2024-08-13 21:45:19 +0700

    landing page n profile

---
+++ src/components/Button.js
@@ -0,0 +1,44 @@
+import Link from "next/link"
+
+const Button = () => {
+  return (
+    <>
+        <div className="flex items-center gap-4">
+                <div className="sm:flex sm:gap-4">
+                  <Link className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                    href="/login">Login</Link>
+
+                  <div className="hidden sm:flex">
+                    <Link
+                      className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                      href="/register"
+                    >
+                      Register
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="block md:hidden">
+                  <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+    </>
+  )
+}
+
+export default Button
