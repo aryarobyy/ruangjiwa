f897e4e5f79832821e2e228ac3a2f77616195c60
Roby Aryanata <robyaryanata12345@gmail.com>
2024-08-16 13:47:23 +0700

    postUser

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,29 +1,23 @@
 import { mongoPostUser, mongoGetAllUser } from "@/mongoMethods/user";
 import { v4 as uuidv4 } from "uuid";
-import bcrypt from "bcryptjs";
-import { uploadImage } from "@/utils/cloudinary";
+import bcrypt, { hash } from "bcryptjs";
 import jwt from 'jsonwebtoken'
 import jwtGenerate from "@/hooks/jwtGenerate";
 
 export const POST = async (req, res) => {
     try {
+        const data = await req.json();
 
         const uuid = uuidv4();
-        const { password, profilePic,...otherData } = data;
-
+        
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(data.password, salt);
 
-        let profilePicUrl = "";
-        if (profilePic) {
-            profilePicUrl = await uploadImage(profilePic);
-        }
+        const newData = {...data, userId: uuid, password: hashedPassword}
+        console.log("DATAAAAA", newData)
 
-        const userData = {...otherData, id: uuid, password: hashedPassword, profilePic: profilePicUrl,
-        };
-
-        jwtGenerate(uuid, res)
-        await mongoPostUser(userId);
+        const token = jwtGenerate(uuid, res)
+        await mongoPostUser(newData);
         return Response.json({ message: "Success", token });
     } catch (error) {
         console.error(error.message);
